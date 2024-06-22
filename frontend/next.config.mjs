47505c802832a742cb949630b76e251c7e5be8d5
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-06-22 11:52:22 +0700

    add template dashboard

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
             {
                 protocol: "https", 
                 hostname: "tailwindui.com"
+            }, 
+            {
+                protocol: "https", 
+                hostname: "lh3.googleusercontent.com"
             }
         ]
     }
