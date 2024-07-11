94bdc9c89b8540ae93b82d74144fd253148767aa
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-07-12 03:40:30 +0700

    add some

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -20,7 +20,7 @@ import CustomInput from "./CustomInput";
 import { authFormSchema } from "../../lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { signIn, signUp } from "../../lib/actions/user.action";
+import { signUp } from "../../lib/actions/user.action";
 import {
   signIn as signInWithNextAuth,
   useSession,
