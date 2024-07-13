cb361f69bd5dc39c66bbb40e0ff8793d26505817
dani <admin@widyawicara.com>
2024-07-13 12:46:34 +0700

    fix dark mode toggle

--- frontend/src/components/atoms/themeToggle.tsx
+++ frontend/src/components/atoms/themeToggle.tsx
@@ -15,17 +15,17 @@ const ThemeToggle = () => {
       <button
         className="rounded-l-full  border-l-[1px] border-y-[1px] items-center dark:border-white border-zinc-900"
         onClick={() => {
-          if (typeof window !== "undefined") {
-            dispatch(
-              updateThemeMode(
-                "system"
-              )
-            );
-          }
+          dispatch(updateThemeMode("system"));
         }}
       >
         <div className="p-1">
-          <ComputerDesktopIcon className={themeValue.mode === "system" ? "text-zinc-900 dark:text-white w-4 h-4": "text-zinc-400 w-4 h-4"} />
+          <ComputerDesktopIcon
+            className={
+              themeValue.mode === "system"
+                ? "text-zinc-900 dark:text-white w-4 h-4"
+                : "text-zinc-400 w-4 h-4"
+            }
+          />
         </div>
       </button>
       <button
@@ -35,7 +35,13 @@ const ThemeToggle = () => {
         }}
       >
         <div className="p-1">
-          <SunIcon className={themeValue.mode === "light" ? "text-zinc-900 dark:text-white w-4 h-4": "text-zinc-400 w-4 h-4"} />
+          <SunIcon
+            className={
+              themeValue.mode === "light"
+                ? "text-zinc-900 dark:text-white w-4 h-4"
+                : "text-zinc-400 w-4 h-4"
+            }
+          />
         </div>
       </button>
       <button
@@ -45,7 +51,13 @@ const ThemeToggle = () => {
         }}
       >
         <div className="p-1">
-          <MoonIcon className={themeValue.mode === "dark" ? "text-zinc-900 dark:text-white w-4 h-4": "text-zinc-400 w-4 h-4"} />
+          <MoonIcon
+            className={
+              themeValue.mode === "dark"
+                ? "text-zinc-900 dark:text-white w-4 h-4"
+                : "text-zinc-400 w-4 h-4"
+            }
+          />
         </div>
       </button>
     </div>
