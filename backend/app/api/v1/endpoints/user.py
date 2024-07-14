c1432376b2bcb99a26960d9e7eaafb2dede42ee0
dani <admin@widyawicara.com>
2024-07-14 12:01:40 +0700

    ongoing google sign in verification from backend

--- backend/app/api/v1/endpoints/user.py
+++ backend/app/api/v1/endpoints/user.py
@@ -1,43 +1,43 @@
 from fastapi import APIRouter, FastAPI, HTTPException, Depends, Request, Response
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from app.core.config import settings 
+from app.core.config import settings
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import datetime 
+import datetime
 from typing import Optional, Annotated
 from app.schemas.email import EmailVerification
-from app.models.user import User 
-from app.schemas.token import RefreshToken 
+from app.models.user import User
+from app.schemas.token import RefreshToken
 from app.crud.user import UserService
 from app.utils.password_utils import get_current_active_user, password_utils
-from app.schemas.google_sign import GoogleSignIn 
-from ....schemas.token import Token 
-from datetime import timedelta 
-from ....core.config import settings, constants 
-from ....utils.password_utils import PasswordUtils  
-import json 
+from app.schemas.google_sign import GoogleSignIn
+from ....schemas.token import Token
+from datetime import timedelta
+from ....core.config import settings, constants
+from ....utils.password_utils import PasswordUtils
+import json
 
 
-router = APIRouter() 
-password_utils = PasswordUtils() 
+router = APIRouter()
+password_utils = PasswordUtils()
 
-user_service = UserService() 
+user_service = UserService()
 
 @router.post('/login')
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],response: Response) -> Token:
     user_data = user_service.get_one_user_by_email(User(email=form_data.username, password=form_data.password))
-    # verify email and password 
+    # verify email and password
 
-    if len(user_data) < 1: 
+    if len(user_data) < 1:
         raise HTTPException(status_code=401, detail="Email not registered. Please sign up")
-    
+
 
     password_verified = password_utils.verify_password(form_data.password, user_data[0].get('password', ''))
-    if password_verified == False: 
+    if password_verified == False:
         raise HTTPException(status_code=401, detail="Incorrect Password")
-    
-    # check user email verification 
-    if user_data[0].get('verified') == False: 
+
+    # check user email verification
+    if user_data[0].get('verified') == False:
         raise HTTPException(status_code=401, detail="Email is not verified")
 
     access_token = password_utils.create_access_token(
@@ -53,13 +53,14 @@ def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],response: R
 
 @router.post("/google-login")
 def login_with_google(google_sign_in: GoogleSignIn):
-    user_data = user_service.get_one_user_by_email(google_sign_in.email)
+    user_email = google_sign_in.email
+    user_data = user_service.get_one_user_by_email(User(email=user_email))
 
-    if len(user_data) > 0: 
-        # login
+    # extract the google auth key results and verify
 
-        # check if email is verified 
-        if (user_data[0].get("verified") == False): 
+    if len(user_data) > 0:
+        # check if email is verified
+        if (user_data[0].get("verified") == False):
             raise HTTPException(status_code=401, detail="Email is not verified")
         access_token = password_utils.create_access_token(
             data={"sub": google_sign_in.email, "email": google_sign_in.email, "token_type": constants.token_type_access_token}
@@ -67,73 +68,65 @@ def login_with_google(google_sign_in: GoogleSignIn):
         )
 
         refresh_token = password_utils.create_access_token(
-            data={"sub": google_sign_in.email, "email": google_sign_in.email, "token_type": constants.token_type_refresh_token}, 
+            data={"sub": google_sign_in.email, "email": google_sign_in.email, "token_type": constants.token_type_refresh_token},
             expires_delta=constants.refresh_token_expires
         )
 
         return JSONResponse(status_code=200, content= {"access_token": access_token, "refresh_token": refresh_token})
-    else: 
-        # register 
+    else:
+        # register
 
-        try: 
+        try:
 
-            new_user = User(email=google_sign_in.email, first_name=google_sign_in.first_name, 
-                            last_name=google_sign_in.last_name, verified=False)
+            new_user = User(email=google_sign_in.email, first_name=google_sign_in.first_name,
+                            last_name=google_sign_in.last_name, verified=True) # for google sign in process, we don't need to verify the email
             registered_user_data_model = user_service.insert_user_during_registration(new_user)
             registered_user = registered_user_data_model.model_dump()
 
-            try: 
-                verification_token = password_utils.create_access_token({"sub": registered_user.get("email"), "email": registered_user.get("email"), 
-                                                                         "token_type": constants.token_type_verification_token}, 
-                                                                       expires_delta=constants.verification_token_expires)
-                registered_user['verification_token'] = verification_token
-            except Exception as e: 
-                raise HTTPException(status_code=500, detail="Unknown Error, Please Try Again or Contact Us")
-
-        except Exception as e: 
+        except Exception as e:
             raise HTTPException(status_code=500, detail="Unknown Error, Please Try Again or Contact Us")
-        
+
         return JSONResponse(status_code=200, content=registered_user)
 
 @router.post("/register")
-def register(user: User): 
-    # check user within database or not 
+def register(user: User):
+    # check user within database or not
     user_array = user_service.get_one_user_by_email(user)
 
     if len(user_array) > 0:
         raise HTTPException(status_code=409, detail="Email already registered")
 
-    # insert new user 
-    registered_user = {} 
-    
-    
-    # encrypt password 
+    # insert new user
+    registered_user = {}
+
+
+    # encrypt password
     user.password = password_utils.get_password_hash(user.password)
 
-    try: 
+    try:
         registered_user_data_model = user_service.insert_user_during_registration(user)
         registered_user = registered_user_data_model.model_dump()
 
-        # create verification token 
-        try: 
-            # 12 hours jwt token 
+        # create verification token
+        try:
+            # 12 hours jwt token
             verification_token = password_utils.create_access_token({'sub': registered_user.get('email'),'email': registered_user.get('email'), 'token_type': constants.token_type_verification_token}, expires_delta=timedelta(hours=12))
-            registered_user["verification_token"] = verification_token 
-        except Exception as e: 
+            registered_user["verification_token"] = verification_token
+        except Exception as e:
             raise Exception(status_code=500, detail="Unknown Error while generating verification token")
 
-    except Exception as e: 
+    except Exception as e:
         raise HTTPException(status_code=500, detail='Unknown Error, Please Try Again or Contact Us')
-    
+
     del registered_user['password']
 
-    return JSONResponse(status_code=200, content=registered_user) 
+    return JSONResponse(status_code=200, content=registered_user)
 
 
 
 @router.get("/me", response_model=User)
-async def get_me(current_user: Annotated[User, Depends(get_current_active_user)],): 
-    return User(id=1, email='norma.risdiandita@gmail.com', password='gitugiut', first_name='normadani', 
+async def get_me(current_user: Annotated[User, Depends(get_current_active_user)],):
+    return User(id=1, email='norma.risdiandita@gmail.com', password='gitugiut', first_name='normadani',
                         last_name='risdiandita', verified=False)
 
 @router.post('/refresh')
@@ -144,11 +137,11 @@ def refresh(token: RefreshToken):
     we can use the get_jwt_subject() function to get the subject of the refresh
     token, and use the create_access_token() function again to make a new access token
     """
-    refresh_token = token.refresh_token 
+    refresh_token = token.refresh_token
 
     payload = password_utils.decode_token(refresh_token, token_type=constants.token_type_refresh_token)
 
-    # check if the email within payload exists 
+    # check if the email within payload exists
 
     access_token = password_utils.create_access_token(
         data={'sub': payload.get('email'), 'email': payload.get('email'), 'token_type': constants.token_type_access_token}, expires_delta=constants.access_token_expires
@@ -157,23 +150,23 @@ def refresh(token: RefreshToken):
     refresh_token_updated = password_utils.create_access_token(
         data={'sub': payload.get('email'), 'email': payload.get('email'), 'token_type': constants.token_type_refresh_token}, expires_delta=constants.refresh_token_expires
     )
-    
+
     return {'access_token': access_token, 'refresh_token': refresh_token_updated}
-    
+
 
 @router.get("/verify/{verification_token}")
-def verify_email(verification_token: str): 
+def verify_email(verification_token: str):
     verification_payload = password_utils.decode_token(verification_token, token_type=constants.token_type_verification_token)
     user_ = user_service.verify_user_by_email(verification_payload.get("email"))
 
 
 @router.post("/send-email-verification")
 def verify(emailVerification: EmailVerification):
-    user_service.send_email_verification(body=emailVerification.body, subject=emailVerification.subject, 
-                            from_email=emailVerification.sender, to_email=emailVerification.recipient) 
-    
+    user_service.send_email_verification(body=emailVerification.body, subject=emailVerification.subject,
+                            from_email=emailVerification.sender, to_email=emailVerification.recipient)
+
     return {'user': 'gitu'}
-    
+
 @router.post("/send-forgot-password-email")
 def send_forgot_password_email(email: str):
     return {'forgot password': 'sent'}
--- frontend/lib/authOptions.ts
+++ frontend/lib/authOptions.ts
@@ -22,8 +22,8 @@ export const authOptions: NextAuthOptions = {
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        console.log('credentials', credentials)
-        console.log("req", req)
+        console.log("credentials", credentials);
+        console.log("req", req);
         let user = {
           name: credentials?.email,
           password: credentials?.password,
@@ -57,47 +57,42 @@ export const authOptions: NextAuthOptions = {
   ],
   pages: { signIn: "/sign-in", error: "/sign-in" },
   callbacks: {
-    async signIn({ user, account, profile, }) { //  email, credentials
+    async signIn({ user, account, profile }) {
+      //  email, credentials
       if (account?.provider === "google") {
+        console.log("account", account);
+        return true;
         if (profile?.email && profile?.email_verified) {
-          // signInWithGoogle 
+          // signInWithGoogle
 
           const signInWithGoogleConfig = {
-            method: HttpMethod.POST, 
+            method: HttpMethod.POST,
             headers: { "Content-Type": "application/json" },
             data: {
-              email: profile?.email, 
-              first_name: profile?.given_name ? profile?.given_name : "Guest", 
-              last_name: profile?.family_name ? profile?.family_name : "Guest", 
+              email: profile?.email,
+              first_name: profile?.given_name ? profile?.given_name : "Guest",
+              last_name: profile?.family_name ? profile?.family_name : "Guest",
             },
-          }
+          };
 
-
-
-          // const backendService = new BackendService(); 
+          // const backendService = new BackendService();
           // const results = await backendService.request("/v1/users/google-login", signInWithGoogleConfig)
 
-          // this needs to be access_token and refresh_token 
-
-
-
-
-
-          
+          // this needs to be access_token and refresh_token
         }
-        return true 
+        return true;
 
-        //         user {   id: '115690064868974581249',                                                                                                                                                               
-        //   name: 'Norma Dani Risdiandita',                                                                                                                                                            
-        //   email: 'norma.risdiandita@gmail.com',                                                       
+        //         user {   id: '115690064868974581249',
+        //   name: 'Norma Dani Risdiandita',
+        //   email: 'norma.risdiandita@gmail.com',
         //   image: 'https://lh3.googleusercontent.com/a/ACg8ocI-oFu2Mkz1jHkqXtFiZjQUckuzwTWrewvXZrY6ZPaevlrqBw=s96-c'
-        // }                                                                                                                                                                                            
-        // account {                           
-        //   provider: 'google',                                                                                                                                                                        
-        //   type: 'oauth',                                                                                                                                                                             
-        //   providerAccountId: '115690064868974581249',                                                                                                                                                
-        //   access_token: '<access_token>',                                                                                                                                             
-        //   expires_at: 1720861951,                                                                                                                                                                    
+        // }
+        // account {
+        //   provider: 'google',
+        //   type: 'oauth',
+        //   providerAccountId: '115690064868974581249',
+        //   access_token: '<access_token>',
+        //   expires_at: 1720861951,
         //   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid',
         //   token_type: 'Bearer',
         //   id_token: '<bearer type token>'
@@ -155,7 +150,7 @@ export const authOptions: NextAuthOptions = {
 
           const results = await backendService.request(
             "/v1/users/refresh",
-            refreshTokenConfig
+            refreshTokenConfig,
           );
           token = results;
         }
@@ -181,4 +176,3 @@ export const authOptions: NextAuthOptions = {
     maxAge: 60 * 60 * 24,
   },
 };
-
