3aa7fdbbe684ef42d27f3e5cd06c3d7524e5f781
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-07-21 06:02:03 +0700

    add user_team

--- backend/app/models/user_team.py
+++ backend/app/models/user_team.py
@@ -1,12 +1,22 @@
 from sqlmodel import Field, SQLModel, Enum
+import enum 
 
+class Access(str, enum.Enum):
+    view = "view"
+    edit = "edit"
+    create = "create"
+    delete = "delete"
+    approve = "approve"
+    review = "review"
 
-class Access()
+class Role(str, enum.Enum): 
+    admin = "admin"
+    member = "member"
 
 class UserTeam(SQLModel, table=True): 
     __tablename__ = "user_team_s"
     id: int | None = Field(default=None, primary_key=True)
     user_id: int | None = Field(default=None, foreign_key="users.id")
     team_id: int | None = Field(default=None, foreign_key="teams.id")
-    role: 
-    access: 
+    role: Access
+    access: Role 
