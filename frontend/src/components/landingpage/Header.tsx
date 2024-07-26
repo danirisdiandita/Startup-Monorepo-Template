6838756357887676b98b717f777d3495559bc63e
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-07-27 06:32:12 +0700

    add header within landing page

--- frontend/src/components/landingpage/Header.tsx
+++ frontend/src/components/landingpage/Header.tsx
@@ -1,31 +1,32 @@
-'use client'
-
-import Link from 'next/link'
+"use client";
+import NextLink from "next/link";
 import {
   Popover,
   PopoverButton,
   PopoverBackdrop,
   PopoverPanel,
-} from '@headlessui/react'
-import clsx from 'clsx'
-import { Container } from './Container'
-import { Logo } from './Logo'
-import { NavLink } from './NavLink'
-import { Button } from './Button'
-
+} from "@headlessui/react";
+import clsx from "clsx";
+import { Container } from "./Container";
+import { NavLink } from "./NavLink";
+import { Button } from "../catalyst/button";
+import Image from "next/image";
+import { useAppSelector } from "@/lib/hooks";
+import { useEffect } from "react";
+import { Text } from "../catalyst/text";
 
 function MobileNavLink({
   href,
   children,
 }: {
-  href: string
-  children: React.ReactNode
+  href: string;
+  children: React.ReactNode;
 }) {
   return (
-    <PopoverButton as={Link} href={href} className="block w-full p-2">
+    <PopoverButton as={NextLink} href={href} className="block w-full p-2">
       {children}
     </PopoverButton>
-  )
+  );
 }
 
 function MobileNavIcon({ open }: { open: boolean }) {
@@ -40,19 +41,19 @@ function MobileNavIcon({ open }: { open: boolean }) {
       <path
         d="M0 1H14M0 7H14M0 13H14"
         className={clsx(
-          'origin-center transition',
-          open && 'scale-90 opacity-0',
+          "origin-center transition",
+          open && "scale-90 opacity-0"
         )}
       />
       <path
         d="M2 2L12 12M12 2L2 12"
         className={clsx(
-          'origin-center transition',
-          !open && 'scale-90 opacity-0',
+          "origin-center transition",
+          !open && "scale-90 opacity-0"
         )}
       />
     </svg>
-  )
+  );
 }
 
 function MobileNavigation() {
@@ -79,33 +80,69 @@ function MobileNavigation() {
         <MobileNavLink href="/login">Sign in</MobileNavLink>
       </PopoverPanel>
     </Popover>
-  )
+  );
 }
 
 export function Header() {
+  const value = useAppSelector((state) => state.theme);
+  useEffect(() => {
+    if (value.mode === "dark") {
+      document.body.classList.add("dark");
+    } else if (value.mode === "system") {
+      if (typeof window !== "undefined") {
+        const theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light";
+
+        if (theme === "dark") {
+          document.body.classList.add("dark");
+        }
+      }
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [value.mode]);
+
+  const isLightOrDark = (mode: "system" | "dark" | "light") => {
+    if (mode === "system") {
+      if (typeof window !== "undefined") {
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light";
+      } else {
+        return "dark";
+      }
+    } else {
+      return mode;
+    }
+  };
+
   return (
-    <header className="py-10">
+    <header className={`py-4 dark:bg-zinc-900 w-full fixed z-50 ${value.mode}`}>
       <Container>
         <nav className="relative z-50 flex justify-between">
           <div className="flex items-center md:gap-x-12">
-            <Link href="#" aria-label="Home">
-              <Logo className="h-10 w-auto" />
-            </Link>
+            <Image
+              width={100}
+              height={100}
+              src={
+                isLightOrDark(value.mode) === "light"
+                  ? "/images/logos/logo.svg"
+                  : "/images/logos/logo-dark.svg"
+              }
+              alt="product_logo"
+            />
             <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#features">Features</NavLink>
+              <Text>Features</Text>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
             </div>
           </div>
-          <div className="flex items-center gap-x-5 md:gap-x-8">
-            <div className="hidden md:block">
-              <NavLink href="/sign-in">Sign in</NavLink>
-            </div>
-            <Button href="/sign-up" color="blue">
-              <span>
-                Get started <span className="hidden lg:inline">today</span>
-              </span>
+          <div className="flex items-center justify-between space-x-2">
+            <Button href={"/sign-in"} outline>
+              Sign In
             </Button>
+            <Button href="/sign-up">Sign Up</Button>
             <div className="-mr-1 md:hidden">
               <MobileNavigation />
             </div>
@@ -113,5 +150,5 @@ export function Header() {
         </nav>
       </Container>
     </header>
-  )
+  );
 }
