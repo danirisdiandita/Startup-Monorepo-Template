9e38533dea76a0961efa20ed788e8a47dc8405ca
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-07-28 05:21:32 +0700

    fix hero

--- frontend/src/components/landingpage/Hero.tsx
+++ frontend/src/components/landingpage/Hero.tsx
@@ -21,7 +21,7 @@ export function Hero() {
             <path d="M.5 200V.5H200" fill="none" />
           </pattern>
         </defs>
-        <svg x="50%" y={-1} className="overflow-visible fill-gray-800/20">
+        <svg x="50%" y={-1} className="overflow-visible fill-white dark:fill-gray-800/20">
           <path
             d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
             strokeWidth={0}
@@ -43,7 +43,7 @@ export function Hero() {
             clipPath:
               "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
           }}
-          className="aspect-[1108/632] w-[69.25rem] bg-white  dark:bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
+          className="aspect-[1108/632] w-[69.25rem] bg-white  dark:bg-gradient-to-r dark:from-[#80caff] dark:to-[#4f46e5] opacity-20"
         />
       </div>
       <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:flex lg:px-8 lg:pt-40">
