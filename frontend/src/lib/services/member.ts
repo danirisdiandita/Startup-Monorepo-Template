57453a0a7eabdebec38788cff37fca463c7b4403
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-08-03 06:58:26 +0700

    ongoing adding member session token

--- frontend/src/lib/services/member.ts
+++ frontend/src/lib/services/member.ts
@@ -1,6 +1,48 @@
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../../lib/authOptions";
 
 
 interface Member {
-    
+    id: number; 
+    user_id: number; 
+    email: string; 
+    first_name: string; 
+    last_name: string; 
+    team_id: number; 
+    team_name: string; 
+    role: string; 
+    access: string; 
 }
+
+const session = await getServerSession(authOptions)
+
+
+const memberApi = createApi({
+    reducerPath: 'memberApi', 
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:8000/api/v1/teams/default-team-members", 
+        prepareHeaders: (headers, {getState}) => {
+            const accessToken = session?.access_token 
+        }
+    }), 
+    endpoints: (builder) => ({
+        getDefaultMembers: builder.query<Member[], void>({
+
+        })
+    })
+}) 
+
+// [
+//     {
+//       "id": 1,
+//       "user_id": 86,
+//       "email": "norma.risdiandita@gmail.com",
+//       "first_name": "Norma Dani",
+//       "last_name": "Risdiandita",
+//       "team_id": 5,
+//       "team_name": "norma.risdiandita@gmail.com",
+//       "role": "admin",
+//       "access": "admin"
+//     }
+//   ]
