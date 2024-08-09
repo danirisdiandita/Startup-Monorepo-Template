f3d6a8b1240de6ed766625e60b8cb5e7b6c9cd21
dani <admin@widyawicara.com>
2024-08-09 07:22:32 +0700

    ongoing send team email invitation 5

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import AuthForm from "@/components/AuthForm";
 import { useSearchParams } from "next/navigation";
 
@@ -7,6 +7,11 @@ import React from "react";
 const SignIn = () => {
   const searchParams = useSearchParams();
   console.log("searchParams", searchParams);
+
+  if (searchParams.get("link")) {
+    console.log('searchParams.get("link")', searchParams.get("link"));
+  
+  }
   return (
     <section className="flex-center size-full max-sm:px-6">
       <AuthForm type="sign-in" />
