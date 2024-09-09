a52ba0865a960197fd95458451aecb34ff73b5dc
Norma Dani Risdiandita <dani@Normas-MacBook-Air-2.local>
2024-09-10 05:12:12 +0700

    add some

--- backend/app/db/seeder.py
+++ backend/app/db/seeder.py
@@ -9,7 +9,7 @@ from app.models.plan import Plan
 from app.models.feature import Feature 
 from app.models.plan_feature import PlanFeature 
 import os 
-
+from datetime import datetime
 with open("./app/models/data/plan.json", "r") as file:
     plan_data = json.load(file)
 
@@ -17,9 +17,22 @@ with open("./app/models/data/features.json", "r") as file:
     feature_data = json.load(file) 
 
 
-plan_data = [Plan(**plan) for plan in plan_data]
-feature_data = [Feature(**feature) for feature in feature_data]
+plan_data = [Plan(
+                    id=idx,
+                    name=plan["name"], 
+                    price=plan["price"], 
+                    description=plan["description"], 
+                    billing_cycle=plan["billing_cycle"], 
+                  created_at=datetime.now()) 
+            for idx, plan in enumerate(plan_data)]
+
+feature_data = [Feature(
+                        id=idx,
+                        name=feature["name"], 
+                        description=feature["description"], 
+                        plans=feature["plans"], 
+                        category=feature["category"],
+                        created_at=datetime.now()) 
+                for idx, feature in enumerate(feature_data)]
 
 
-print(plan_data)
-print(feature_data)
--- backend/app/models/feature.py
+++ backend/app/models/feature.py
@@ -10,4 +10,5 @@ class Feature(SQLModel, table=True):
     name: str | None = Field(default=None)
     description: str | None = Field(default=None)
     created_at: datetime | None = Field(default=datetime.now())
+    category: str | None = Field(default=None)
     
